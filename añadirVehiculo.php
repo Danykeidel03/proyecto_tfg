1c0d99f79f06d4384b3866f57e51edff71a82ad8
Daniel <danykeidel03@gmail.com>
2023-04-19 16:50:23 +0200

    formulario hehco excepto foto

--- añadirVehiculo.php
+++ añadirVehiculo.php
@@ -67,8 +67,29 @@
     #km,
     #matricula,
     #precio {
-        margin-bottom: 20px;
-        width: 400px;
+        display: flex;
+        width: 695px;
+        padding: 10px;
+        background-color: #f7f7f7;
+        color: #333;
+        border: none;
+        border-radius: 4px;
+        margin-top: 15px;
+    }
+
+    #marca,
+    #combustible,
+    #cambio,
+    #puertas {
+        margin-left: 8px;
+        width: 711px;
+        height: 44px;
+        margin-top: 15px;
+    }
+
+
+    #foto {
+        background-color: white;
     }
 
     html,
@@ -89,11 +110,79 @@
     }
 
     #anuncio {
-        background-color: #afafaf;
+        /* background-color: #afafaf; */
+        border: 1px black solid;
+        border-radius: 6px;
         margin: auto;
-        width: 1500px;
-        height: 900px;
-        margin-top: 150px;
+        width: 800px;
+        height: 1900px;
+        margin-top: 130px;
+        -webkit-box-shadow: 6px 7px 13px -4px rgba(0, 0, 0, 0.55);
+        -moz-box-shadow: 6px 7px 13px -4px rgba(0, 0, 0, 0.55);
+        box-shadow: 6px 7px 13px -4px rgba(0, 0, 0, 0.55);
+    }
+
+    form {
+        margin-left: 5%;
+    }
+
+    #subir_coche {
+        display: inline-block;
+        padding: 10px 20px;
+        background-color: #f7f7f7;
+        color: #333;
+        border: none;
+        border-radius: 4px;
+        text-align: center;
+        text-decoration: none;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+        height: 44px;
+        margin-top: 15px;
+    }
+
+    #subir_coche:hover {
+        background-color: #7f0000;
+        color: white;
+    }
+
+    #subir_coche:active {
+        background-color: #d9d9d9;
+    }
+
+    textarea {
+        display: block;
+        width: 695px;
+        height: 150px;
+        padding: 10px;
+        background-color: #f7f7f7;
+        color: #333;
+        border: none;
+        border-radius: 4px;
+        resize: none;
+        margin-left: 8px;
+        margin-top: 15px;
+
+    }
+    #foto{
+        display: inline-block;
+      padding: 10px;
+      background-color: #f7f7f7;
+      color: #333;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+
+    #foto1{
+        display: inline-block;
+      margin-left: -2px;
+      vertical-align: middle;
+    }
+
+    #volver{
+        margin-top: 20px;
+        margin-left: 47px;
     }
 </style>
 <?php
@@ -148,9 +237,9 @@ include_once('footer/header.php')
             </select><br>
             <label id="puertas1">Numero de Puertas</label>
             <select id="puertas" name="puertas">
-                <option value='3'>3</option>
-                <option value='4'>4</option>
-                <option value='5'>5</option>
+                <option value='3'>3('Dos puertas + maletero')</option>
+                <option value='4'>4('Tres puertas + maletero')</option>
+                <option value='5'>5('Cuatro puertas + maletero')</option>
             </select><br>
             <label id="color_ext1">Color exterior</label>
             <input type="text" class="color_ext" name="color_ext" id="color_ext"><br>
@@ -166,17 +255,11 @@ include_once('footer/header.php')
             <textarea id="descripcion" name="descripcion" rows="4" cols="50">
             </textarea>
             <br>
-            <input id="subir_coche" class="subir_coche" name="subir_coche" type="submit" value="Subir Coche">
-            <br>
+            <input name='subir_coche' id='subir_coche' class='subir_coche' type="submit" value="Subir Coche">
         </form>
-
+        <a href="catalogo.php" id="volver">VOLVER</a>
     </div>
-    <br><br><br><br><br><br><br><br>
-
-    <a href="catalogo.php">VOLVER</a>
-
-
-
+    <br><br><br><br><br>
     <footer id="footer"></footer>
 </body>
 <script src="footer/añadirheadersfooters.js"></script>
@@ -232,7 +315,7 @@ include_once('footer/header.php')
         let puertas = puertasImput.value;
 
         let id = localStorage.getItem('id')
-        let usernme = localStorage.getItem('usernme')
+        let username = localStorage.getItem('usernme')
 
         if (modelo != "" && acabado != "" && ano != "" && cv != "" && color_ext != "" && color_int != "" && matricula != "" && km != "" && precio != "" && descripcion != "") {
             let coche = {
@@ -262,7 +345,7 @@ include_once('footer/header.php')
                     headers: {
                         'Content-Type': 'application/json;charser=utf-8'
                     },
-                    body: rutaJson
+                    body: cocheJson
                 })
 
                 .then(response => {
